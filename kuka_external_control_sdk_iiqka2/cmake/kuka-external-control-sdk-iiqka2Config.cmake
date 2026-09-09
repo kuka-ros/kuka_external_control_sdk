2090286f69b1e3d6a66536d58266777f6d1dbbce
Svastits <svastits1@gmail.com>
2026-09-09 08:36:46 +0200

    fix: update iiqka2 cmake config for new target and tinyxml2 dependency

--- kuka_external_control_sdk_iiqka2/cmake/kuka-external-control-sdk-iiqka2Config.cmake
+++ kuka_external_control_sdk_iiqka2/cmake/kuka-external-control-sdk-iiqka2Config.cmake
@@ -1,5 +1,6 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(Threads REQUIRED)
+find_dependency(tinyxml2 REQUIRED)
 
-include("${CMAKE_CURRENT_LIST_DIR}/kuka-kss-client-libraryTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/kuka-external-control-sdk-iiqka2Targets.cmake")
