5b5ccaccfb367ce95c7b379a9fe35859927a31d6
Svastits <svastits1@gmail.com>
2026-09-09 08:52:55 +0200

    fix: use underscore Targets filename in common config include

--- kuka_external_control_sdk_common/cmake/kuka_external_control_sdk_commonConfig.cmake
+++ kuka_external_control_sdk_common/cmake/kuka_external_control_sdk_commonConfig.cmake
@@ -2,4 +2,4 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(Threads REQUIRED)
 
-include("${CMAKE_CURRENT_LIST_DIR}/kuka-external-control-sdk-commonTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/kuka_external_control_sdk_commonTargets.cmake")
