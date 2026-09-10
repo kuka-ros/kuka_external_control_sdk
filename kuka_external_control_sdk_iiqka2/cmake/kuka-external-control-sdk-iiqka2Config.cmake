8d41606dd02cd057e1a0a5c79ab6ceee9a7d3e23
Svastits <svastits1@gmail.com>
2026-09-10 09:55:21 +0200

    cmake fix

--- kuka_external_control_sdk_iiqka2/cmake/kuka-external-control-sdk-iiqka2Config.cmake
+++ kuka_external_control_sdk_iiqka2/cmake/kuka-external-control-sdk-iiqka2Config.cmake
@@ -2,5 +2,6 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(Threads REQUIRED)
 find_dependency(tinyxml2 REQUIRED)
+find_dependency(kuka_external_control_sdk_common REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/kuka-external-control-sdk-iiqka2Targets.cmake")
