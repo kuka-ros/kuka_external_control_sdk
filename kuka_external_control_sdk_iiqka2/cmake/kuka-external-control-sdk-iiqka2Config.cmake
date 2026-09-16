6284eb9eeebf7291a88f6a67bbbe2c62cda06e0e
Svastits <svastits1@gmail.com>
2026-09-16 11:21:12 +0200

    tinyxml fix

--- kuka_external_control_sdk_iiqka2/cmake/kuka-external-control-sdk-iiqka2Config.cmake
+++ kuka_external_control_sdk_iiqka2/cmake/kuka-external-control-sdk-iiqka2Config.cmake
@@ -1,7 +1,11 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(Threads REQUIRED)
-find_dependency(tinyxml2 REQUIRED)
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+find_package(tinyxml2 QUIET)
+if(NOT tinyxml2_FOUND)
+  find_dependency(tinyxml2_vendor REQUIRED)
+endif()
 find_dependency(kuka_external_control_sdk_common REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/kuka-external-control-sdk-iiqka2Targets.cmake")
